b3b24166cec1ad4ce9abb18a63766e8e4ddaff20
Frost-704 <boolean.pg.me@gmail.com>
2023-12-26 01:36:16 +0300

    feat: add stack task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,16 +6,40 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class BinarySearchTree {
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree {
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.root = new Node(data);
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newData = new Node(data);
+
+    const move = (node) => {
+      if (data < node.value) {
+      if(node.left === undefined) {
+        node.left = newData;
+      } 
+      } else {
+        move(node.left)
+      }
+      if (data > node.value) {
+        if (node.right === undefined) {
+          node.right = newData;
+        }
+        else {
+          move(node.right)
+        }
+    }
+    }
+    this.move(this.root)
   }
 
   has(/* data */) {
